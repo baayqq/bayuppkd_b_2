71a4451597a7a918ca23b938fcf48f0de33d2589
baayqq <bebenwork@hmail.com>
2025-05-20 10:33:29 +0700

    change colors icon

---
+++ lib/meet_1/meet_2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class meetDua extends StatelessWidget {
+  const meetDua({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PERTEMUAN 2"),
+        backgroundColor: Colors.greenAccent,
+        centerTitle: true,
+      ),
+      
+    );
+  }
+}
--- lib/meet_1/tugas_1_flutter.dart
+++ lib/meet_1/tugas_1_flutter.dart
@@ -11,19 +11,23 @@ class tugasDua extends StatelessWidget {
         backgroundColor: Colors.tealAccent,
         centerTitle: true,
       ),
+      drawer: Drawer(
+        child: Container(child: Column(children: [Text("login")])),
+      ),
       body: Column(
         //crossAxisAlignment: CrossAxisAlignment.start,
         //mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Icons.account_circle_outlined, size: 50),
-          Text("Nama: Bayu", style: TextStyle(fontSize: 20)),
+          SizedBox(height: 50),
+          Icon(Icons.account_circle_outlined, color: Colors.green, size: 190),
+          Text("Nama : Bayu", style: TextStyle(fontSize: 30)),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [Icon(Icons.location_on), Text("Jakarta")],
           ),
           Text(
             "Seorang pelajar yang sedang belajar Flutter",
-            style: TextStyle(fontSize: 10),
+            style: TextStyle(fontSize: 15),
           ),
         ],
       ),
