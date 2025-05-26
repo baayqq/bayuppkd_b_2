74bb0235b4d11893020dca31cefe67b9fa9131b6
baayqq <bebenwork@hmail.com>
2025-05-26 11:36:06 +0700

    tugas 6 flutter

--- lib/meet_6/tugas_6.dart
+++ lib/meet_6/tugas_6.dart
@@ -11,6 +11,8 @@ class _TugasEnamState extends State<TugasEnam> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset:
+          false, //hilangin overfloow saat onsreen keyboard
       appBar: AppBar(
         leading: IconButton(
           onPressed: () {},
@@ -22,98 +24,212 @@ class _TugasEnamState extends State<TugasEnam> {
         //actions: [Icon(Icons.arrow_back_ios_new),],
         title: Text(
           "Login",
-          style: TextStyle(color: Colors.white, fontSize: 20),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         centerTitle: true,
         backgroundColor: Color(0xff00224F),
       ),
 
       backgroundColor: Color(0xff00224F),
-      body: Column(
-        children: [
-          SizedBox(height: 60),
-          Column(
-            children: [
-              Text(
-                "Hello Welcome Back",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25, color: Colors.white),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Welcome back please \n sign in again",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white60),
-              ),
-            ],
-          ),
-          SizedBox(height: 120),
-          Row(
-            children: [
-              Expanded(
-                // width: 20,
-                // height: 80,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          prefixIcon: Icon(
-                            Icons.email,
-                            color: Color(0xffFFFFFF),
-                          ),
-                          hintText: ("email"),
-                          hintStyle: TextStyle(color: Color(0xffFFFFFF)),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xffC4C4C4)),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          prefixIcon: Icon(
-                            Icons.lock,
-                            color: Color(0xffFFFFFF),
-                          ),
-                          hintText: ("paswword"),
-                          hintStyle: TextStyle(color: Color(0xffFFFFFF)),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xffC4C4C4)),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            SizedBox(height: 60),
+            Column(
+              children: [
+                Text(
+                  "Hello Welcome Back",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Welcome back please \n sign in again",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white60),
+                ),
+              ],
+            ),
+            SizedBox(height: 110),
+            Row(
+              children: [
+                Expanded(
+                  // width: 20,
+                  // height: 80,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextField(
+                          textInputAction: TextInputAction.next,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Color(0xffC4C4C4),
+                            ),
+                            hintText: ("Email"),
+                            hintStyle: TextStyle(color: Color(0xffFFFFFF)),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0x30c4c4c4)),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0x30c4c4c4)),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextField(
+                          obscureText: true,
+                          obscuringCharacter:
+                              "*", // mengubah karakter untuk menyembunyikan pasword
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Color(0xffC4C4C4),
+                            ),
+                            // suffixIcon: IconButton(
+                            //   onPressed: () {},
+                            //   //icon: Icon(Icons.remove_red_eye),
+                            // ),
+                            hintText: ("Password"),
+                            hintStyle: TextStyle(color: Color(0xffFFFFFF)),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0x30c4c4c4)),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0x30c4c4c4)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: (Colors.white),
+                padding: EdgeInsets.symmetric(horizontal: 140, vertical: 16),
+              ),
+
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: (Colors.white),
-              padding: EdgeInsets.symmetric(horizontal: 110, vertical: 16),
+            ),
+            SizedBox(height: 70),
+            Row(
+              children: [
+                Expanded(child: Divider(color: Color(0x40C4C4C4))),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12),
+                  child: Text("or", style: TextStyle(color: Color(0xffFFFFFF))),
+                ),
+                Expanded(child: Divider(color: Color(0x40C4C4C4))),
+              ],
             ),
 
-            child: Text(
-              "Login",
-              style: TextStyle(color: Colors.black, fontSize: 20),
+            SizedBox(height: 20),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff11325C),
+                    padding: EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/fb.png"),
+                      SizedBox(width: 8),
+                      Text(
+                        "Facebook",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: 12),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff11325C),
+                    padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/google.png"),
+                      SizedBox(width: 8),
+                      Text(
+                        "Gmail",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 50),
-          Text("or", style: TextStyle(color: Colors.white)),
-        ],
+            SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: 'Already have an account ?',
+                    style: TextStyle(fontSize: 14, color: Color(0xff888888)),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
